df171dd366c844f9cbe1e44fb8ca860934618101
James Pier <jpier2012@gmail.com>
2019-08-12 18:57:28 -0500

    stub restaurant class and renderRestaurant() method

--- app/assets/javascripts/restaurant.js
+++ app/assets/javascripts/restaurant.js
@@ -12,10 +12,67 @@ $(function() {
     .then(restaurants => {
       index.innerHTML += "<ul>"
         restaurants.map(restaurant => {
-          index.innerHTML += `<li>${restaurant.name}</li>`;
+          newR = new Restaurant(restaurant)
+          index.innerHTML += "<li>"
+          index.innerHTML += newR.renderRestaurant();
+          index.innerHTML += "</li>";
       });
       index.innerHTML += "</ul>"
     });
   });
+
+  class Restaurant {
+    constructor(restaurant) {
+      this.name = restaurant.name
+      this.address = restaurant.address
+      this.cuisine = restaurant.cuisine
+      this.dress_code = restaurant.dress_code
+      this.outdoor_seating = restaurant.outdoor_seating
+      this.child_friendly = restaurant.child_friendly
+      this.open_bar = restaurant.open_bar
+      this.byob = restaurant.byob
+      this.created_by = restaurant.created_by
+    }
+
+    renderRestaurant() {
+      return `
+      <table>
+        <tr>
+            <td>Name: </td>
+            <td>${this.name}</td>
+        </tr>
+        <tr>
+            <td>Address: </td>
+            <td>${this.address}</td>
+        </tr>
+        <tr>
+            <td>Cuisine: </td>
+            <td>${this.cuisine}</td>
+        </tr>
+        <tr>
+            <td>Dress Code:</td>
+            <td>${this.dress_code}</td>
+        </tr>
+        <tr>
+            <td>Outdoor Seating: </td>
+            <td>${this.outdoor_seating}</td>
+        </tr>
+        <tr>
+            <td>Child Friendly: </td>
+            <td>${this.child_friendly}</td>
+        </tr>
+        <tr>
+            <td>BYOB: </td>
+            <td>${this.byob}</td>
+        </tr>
+        <tr>
+            <td>Open Bar: </td>
+            <td>${this.open_bar}</td>
+        </tr>
+      </table>      
+      `
+    }
+
+  }
 
 });
